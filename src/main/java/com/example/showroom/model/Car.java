c4b3570965efbee6eeb09b647d101186ff722be5
“Volodislav89” <shbloodislav@gmail.com>
2020-06-16 12:15:49 +0300

    SR-2

---
+++ src/main/java/com/example/showroom/model/Car.java
@@ -0,0 +1,22 @@
+package com.example.showroom.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String color;
+    private int year;
+}
